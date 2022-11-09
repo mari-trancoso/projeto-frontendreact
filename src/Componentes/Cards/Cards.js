b3f4f730257b351a905ec5eca5a929dfaa743427
mari-trancoso <mnegraotrancoso@gmail.com>
2022-11-09 16:45:57 -0300

    arrumando a estilização

--- src/Componentes/Cards/Cards.js
+++ src/Componentes/Cards/Cards.js
@@ -1,23 +1,26 @@
 import React from "react";
-import {Card, ImagemPlanetas} from "./style";
+import {Card, ImagemPlanetas, BotaoCarrinho, TextoCard} from "./style";
 import carrinhoImagem from "../../imagem/imagem-carrinho.svg"
 
 function Cards (props) {
     
-    return  <>
-                <Card>
-                    <ImagemPlanetas src={props.planeta.imagem} alt="imagem de planeta"/>
-                    <p>{`Id: ${props.planeta.id}`}</p>
-                    <p>{`Planeta: ${props.planeta.planeta}`}</p>
-                    <p>{`Descrição: ${props.planeta.descricao}`}</p>
-                    <p>{`Preço: ${props.planeta.preco}`}</p>
-                    <p>{`Tempo de Viagem: ${props.planeta.tempoDeViagem}`}</p>
-                    <button onClick={() =>props.addViagem(props.planeta)}>
-                        <img src={carrinhoImagem} alt="" />
-                    </button>
-                </Card>
-                
-            </>
+    return  (
+        <Card>
+            <ImagemPlanetas src={props.planeta.imagem} alt="imagem de planeta"/>
+            <TextoCard>
+                <p>{`Id: ${props.planeta.id}`}</p>
+                <p>{`Planeta: ${props.planeta.planeta}`}</p>
+                <p>{`Descrição: ${props.planeta.descricao}`}</p>
+                <p>{`Preço: ${props.planeta.preco}`}</p>
+                <p>{`Tempo de Viagem: ${props.planeta.tempoDeViagem}`}</p>
+            </TextoCard>
+            <BotaoCarrinho>
+                <button onClick={() =>props.addViagem(props.planeta)}>
+                    <img src={carrinhoImagem} alt="" />
+                </button>
+            </BotaoCarrinho>
+        </Card>
+    )
 }
 
 export default Cards
--- src/styled.js
+++ src/styled.js
@@ -2,10 +2,18 @@ import styled from "styled-components";
 
 const Main = styled.section `
     margin-top: 30px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    grid-template-rows: 1fr;
 
 `
 
 export default Main
+
+export const CardContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding: 24px;
+`
